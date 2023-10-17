ae8fea7a3a964a2730d11fa0c13fa05e1b38a3e1
carldamey <carldamey@gmail.com>
2023-10-17 13:40:53 -0700

    fixes bug regarding router object regexp for todos

--- routes/todos.js
+++ routes/todos.js
@@ -1,8 +1,10 @@
 var express = require("express")
 var router = express.Router()
-var todosCtrl = require("../controllers")
+var todosCtrl = require("../controllers/todos")
 
 // All actual Paths begin with /todos
-router.get()
+router.get("/", function (req, res) {
+    res.send("respond with a resource")
+})
 
 module.exports = router
